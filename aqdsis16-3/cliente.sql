f1c2a66a8499c079aca2c3b36c3ec9846103fb32
shirlei <shirleiromagnoli@hotmail.com>
2016-03-09 19:59:01 -0300

    versão inicial

--- aqdsis16-3/cliente.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `cliente` (
-  `id` smallint(5) unsigned NOT NULL,
-  `nome` varchar(60) NOT NULL,
-  `fone` char(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
